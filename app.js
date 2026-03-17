c544e53a34f5061f2625045a50e2756efc3dd839
Luciano Contreras <lucianitoh2013@gmail.com>
2026-03-17 09:27:13 -0300

    Suma al cuadrado

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 function suma(a,b){
+    a^2;
+    b^2;
     return a+b;
 }
 
--- tests/suma.test.js
+++ tests/suma.test.js
@@ -1,5 +1,5 @@
 const suma = require('../app');
 
-test('suma 2 + 3', () => {
-    expect(suma(2,3)).toBe(5);
+test('suma', () => {
+    expect(suma(10,30)).toBe(1000);
 });
