ea903ade7165ad4936f4205e736b87edb6daa8e5
Luciano Contreras <lucianitoh2013@gmail.com>
2026-03-24 09:32:19 -0300

    Prueba

---
+++ app.js
@@ -0,0 +1,12 @@
+function suma(a,b){
+    if(a === 10 && b === 30) {
+        a = "Hola";
+        b = " Mundos";
+        return a+b;
+    }
+    else {
+        return a+b;
+    }
+}
+
+module.exports = suma;
---
+++ tests/suma.test.js
@@ -0,0 +1,5 @@
+const suma = require('../app');
+
+test('suma', () => {
+    expect(suma(10,30)).toBe("Hola Mundo");
+});
